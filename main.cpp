2819b1d0421b4efbf417886fb0ed8fe7abebfe72
JuanGA12 <juan.galvez@utec.edu.pe>
2020-04-09 21:46:04 -0500

    TERMINADO

--- main.cpp
+++ main.cpp
@@ -6,22 +6,28 @@ using namespace std;
 int main(){
     
     string text;
-    bool quote = true;
+    int quote = 0;
 
     while(getline(cin,text)) {
 
         for (int i = 0; i<text.size();++i) {
 
             if (text[i] == '"') {
-                if (quote)
-                    text.replace(i,1," ``");
-                else
-                    text.replace(i,1," ''");
-                quote = !quote;
+
+                if (quote % 2 == 0) {
+                    text.replace(i, 1, " ``");
+                }
+
+                else{
+                   text.replace(i,1," ''");
+                }
+
+                quote += 1;
             }
             else
                 cout << text[i];
-        }cout << endl;
+        }
+        cout << endl;
     }
     return EXIT_SUCCESS;
 }
